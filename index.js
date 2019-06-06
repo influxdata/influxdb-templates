860d6a36e0e3c97ad1cd676e13c02f5fe6cf247e
Brandon Farmer <bthesorceror@gmail.com>
2019-06-06 16:35:44 -0700

    Expose new templates
    - Redis
    - Nginx
    - Docker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
   system: require('./templates/system.json'),
   gettingStarted: require('./templates/gettingStarted.json'),
-  localMetrics: require('./templates/localMetrics.json')
+  localMetrics: require('./templates/localMetrics.json'),
+  redis: require('./templates/redis.json'),
+  docker: require('./templates/docker.json'),
+  nginx: require('./templates/nginx.json')
 }
