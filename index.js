78b47f19b318d2aaf631ab10c1ea77d575a90d99
Russ Savage <russorat@users.noreply.github.com>
2019-08-12 12:34:36 -0700

    updating the name in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export { default as docker } from "./templates/docker.json";
 export { default as gettingStarted } from "./templates/gettingStarted.json";
 export { default as github } from "./templates/github.json";
 export { default as kubernetes } from "./templates/kubernetes.json";
-export { default as localMetrics } from "./templates/localMetrics.json";
+export { default as ossMetrics } from "./templates/ossMetrics.json";
 export { default as nginx } from "./templates/nginx.json";
 export { default as redis } from "./templates/redis.json";
 export { default as system } from "./templates/system.json";
